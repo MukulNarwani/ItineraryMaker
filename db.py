ae64d6a83ed52733d54da8f36e6c3cd9c1e2667a
Mukul Narwani <narwanimukul103@gmail.com>
2023-09-01 04:43:35 +0400

    Converts to pymongo database

--- db.py
+++ db.py
@@ -4,29 +4,45 @@ import csv
 import os
 import glob
 from model import *
+from pymongo import MongoClient
+
+
 
 class ItineraryDB():
-    def __init__(self,path=os.getcwd()):
-        self.db_path=path
-
-    def cityCSVSaver(self,city):
-        lines=[]
-        if not(f'{city.location}_City' in os.listdir(os.getcwd())):
-            city_csv_line=city.get_csv_line()
-            lines.append(city_csv_line)
-        city_activities=city.loActivity
-        # csvfile.write(os.linesep)
-        for activity in city_activities:
-            lines.append(activity.get_csv_line())
-            # csvfile.write(os.linesep)
-        print(lines)
-        with open(f'{self.db_path}/{city.location}_City','a') as csvfile:
-            writer=csv.writer(csvfile,delimiter='\n')
-            writer.writerow(lines)
     
-    @staticmethod
-    def get_cities():
-        return  glob.glob(os.getcwd()+'/*_City')
-    @staticmethod
-    def load(city):
-        return City.city_from_csv(city)
+    def __init__(self,HOST="localhost",PORT=27017):
+        self.client = MongoClient(HOST, PORT)
+        self.database= self.client['Itinerary']
+      
+    #--create
+    def add_country(self,country):
+        self.database.createCollection(country.strip().lower())
+    def add_city(self,city):
+        self.database.insert_one(city.document)
+    #--read
+    def get_countries(self):
+        return self.database.collection_names()
+    
+    def get_cities(self,country):
+        if not(country in self.get_countries()):
+            raise ValueError(f'{country} not in Countries ') 
+        #[x['City'] for x in list(self.database[country].find())]
+        return list(self.database[country].find())
+    
+    def get_city(self,country,city):
+        #TODO: Check exists?
+        return list(self.database[country].find({'City':city}))
+    #--update
+    def update_city(self,country,city):
+        if not(country in self.get_countries()):
+            raise ValueError(f'{country} not in Countries ') 
+        self.database[country].find_one_and_update({'City':city.location},
+                                                   {'$set':{'coordinates':city.coordinates,'activities':city.activities}},
+                                                   projection = { "coordinates" : 1, "activities" : 1 })
+    #--delete
+    # delete country
+    # delete city 
+    
+#TODO Class TransportDB?
+#TODO Class layers?
+#TODO SCHEMA validation
--- model.py
+++ model.py
@@ -6,51 +6,33 @@ import csv
 
 
 class Location(metaclass=abc.ABCMeta):
-    def __init__(self,location,googleMapLocation):
+    def __init__(self,location,coordinates):
         self.location=location
-        self.googleMapLocation=googleMapLocation
+        self.coordinates=coordinates
     @abc.abstractmethod
     def get_csv_line(self):
-        return f'{self.location}, {self.googleMapLocation}'
+        return f'{self.location}, {self.coordinates}'
     
     
 class City(Location):
     #TODO CATCH ERRORS
-    def __init__(self,location,googleMapLocation,**loActivity):
-        super().__init__(location,googleMapLocation)
+    def __init__(self,location,coordinates,**loActivity):
+        super().__init__(location,coordinates)
         if(type(loActivity)==list):
             self.loActivity=loActivity
         else:
             self.loActivity=list()
             
-    def get_csv_line(self):
-        return super().get_csv_line()
-    
     def append_activity(self,activity):   
         self.loActivity.append(activity)
-        
-    @staticmethod
-    def city_from_csv(city_file):
-        with open(city_file,'r') as f:
-            header=f.readline()
-            activites_csv=f.readlines()
-        city_headers=[x.strip() for x in header.split(', ')]
-        print(city_headers)
-        activities=[]
-        for line in activites_csv:
-            if len(line.strip()) > 0:
-                activities.append(Activity.from_csv(line))    
-    
-        return City(*city_headers,loActivity=activities)
+
                         
 class Activity(Location):
     #TODO CATCH ERRORS
-    def __init__(self,title,googleMapLocation,cost,):
-        super().__init__(title,googleMapLocation)
+    def __init__(self,title,coordinates,cost,):
+        super().__init__(title,coordinates)
         self.cost=cost
-    
-    def get_csv_line(self):
-        return super().get_csv_line() + f', {self.cost}'
+
     @staticmethod
     def from_csv(csv_line):
         #Throw catche rrror here
