8d034f110c38a2395900f48a21ab377387d84e2c
MukulNarwani <narwanimukul103@gmail.com>
2023-10-18 23:27:47 +0400

    Adds tests for Model classes, and BUGFIX

--- model.py
+++ model.py
@@ -3,99 +3,149 @@ import pandas as pd
 import json
 from dataclasses import dataclass
 from db import ItineraryDB
+from typing import List
 
-@dataclass
+
 class Location():
-    title:str
-    coordinates:tuple[int,int]
+    # TODO add coord limitations
+    min_coord = 0
+
+    def __init__(self, title: str, coordinates: tuple[int, int]):
+        if title == "" or title == None:
+            raise TypeError("Title can't be empty")
+        if any(coord < self.min_coord for coord in coordinates):
+            raise ValueError("Invalid Coordinates")
+        self.title = title
+        self.coordinates = coordinates
+
     def __iter__(self):
-        tmp={'title':self.title,'coordinates':self.coordinates}
+        tmp = {'title': self.title, 'coordinates': self.coordinates}
         for key, value in tmp.items():
-            yield key,value
+            yield key, value
+
     def __dict__(self):
-        return {'title':self.title,'coordinates':self.coordinates}
+        return {'title': self.title, 'coordinates': self.coordinates}
+
     def __str__(self):
-        return f'Location:{self.title}, coordinates:{self.coordinates}'
+        return f'Location: {self.title}, coordinates:{self.coordinates}'
+
+    def to_json(cls) -> str:
+        return json.loads(json.dumps(dict(cls)))
+
+
 class Activity():
-    def __init__(self,title,coordinates,cost,):
-        self.cost=cost
-        self.location=Location(title,coordinates)
-    def __str__(self):
+    # TODO allow lists of location
+
+    def __init__(self, title, coordinates, cost,):
+        if cost == None:
+            raise TypeError('Need a value for cost')
+        if cost < 0:
+            raise ValueError('Cost can\'t be negative')
+        self.cost = cost
+        self.location = Location(title, coordinates)
+
+    def __repr__(self):
         return f'{self.location}, cost:{self.cost}'
+
     def __iter__(self):
-        tmp={'location':dict(self.location),'cost':self.cost}
+        tmp = {'location': dict(self.location), 'cost': self.cost}
         for key, value in tmp.items():
-            yield key,value
+            yield key, value
+
     def __dict__(self):
-        return {'title':self.location,'cost':self.cost}
+        return {'title': self.location, 'cost': self.cost}
+
+    def to_json(cls) -> str:
+        return json.loads(json.dumps(dict(cls)))
+
     @staticmethod
     def from_dic(doc):
-        return Activity(doc['title'],doc['coordinates'],doc['cost'])
-    
-@dataclass
-class City():
-    location: Location
-    loActivity: list[Activity]
+        return Activity(doc['location']['title'],
+                        doc['location']['coordinates'],
+                        doc['cost'])
 
-    # def __post_init__(self):
-        # if type(self.loActivity)==Activity:
-        #     self.loActivity=list(self.loActivity)
-    def append_activity(self,activity):   
+
+class City():
+
+    def __init__(self, title, coordinates, loActivity):
+        self.loActivity = loActivity
+        if type(loActivity) != type(list()):
+            self.loActivity = [loActivity]
+        self.location = Location(title, coordinates)
+        # print(type(loActivity))
+        # print(type(self.loActivity))
+
+    def append_activity(self, activity):
         self.loActivity.append(activity)
+
+    def __repr__(cls):
+        activities_str = ', '.join([str(activity)
+                                    for activity in cls.loActivity])
+        return f'City: {cls.location}, Activities: [{activities_str}]'
+
     def __dict__(cls):
-        print('called')
-        return {'City':{'Location':dict(cls.location),
-                        'Activities':[dict(activity) 
+        return {'City': {'Location': dict(cls.location),
+                         'Activities': [dict(activity)
                                         for activity in cls.loActivity]}}
+
     def __iter__(self):
-        for key,value in self.__dict__().items():
+        for key, value in self.__dict__().items():
             yield key, value
+
     @staticmethod
-    def from_dic(doc)->object:
-        for activity in doc['activities']:
-            Activity.from_dic(activity)
-        return City(Location(doc['title'],doc['coord']))
-    def to_json(cls)->str:
-        tmp=','.join([str(x) for x in cls.loActivity])
-        class_str=f'{str(cls.location)}, Activity:{tmp} '
-        return class_str
+    def from_dic(doc) -> object:
+        activities = []
+        doc = doc['City']
+        loc = doc['Location']
+        for activity in doc['Activities']:
+            activities.append(Activity.from_dic(activity))
+        return City(loc['title'], loc['coordinates'], activities)
+
+    def to_json(cls) -> str:
+        return json.loads(json.dumps(dict(cls)))
+
 
 class Country:
-    
-    
-    def __init__(self,cities: str) -> None:
-        self.cities=[]
+
+    def __init__(self, cities: str) -> None:
+        self.cities = []
         self.parse_json(cities)
-    def parse_json(self,json_str):
-        json_cities=json.loads(json_str)
+
+    def parse_json(self, json_str):
+        json_cities = json.loads(json_str)
         for city in json_cities:
             self.cities.append(City.from_dic(city))
-    
+
+
 class Model():
 
     def __init__(self) -> None:
         self.db = ItineraryDB()
-        self.activeCountry=None
+        self.activeCountry = None
 
-    def set_active_country(cls,country)->None:
-        if(country in cls.db.get_countries()):
-            #load country
-            cities=cls.db.get_cities(country)
+    def set_active_country(cls, country) -> None:
+        if (country in cls.db.get_countries()):
+            # load country
+            cities = cls.db.get_cities(country)
             Country(cities)
         else:
             cls.db.add_country(country)
-        
-    def list_handler(cls,country=None,city=None):
+
+    def list_handler(cls, country=None, city=None):
         if not country:
             return cls.db.get_countries()
         elif city:
-            cls.db.get_city(country,city)
+            cls.db.get_city(country, city)
         else:
             cls.db.get_cities(country)
-    def add_handler(cls,city):
-        cls.db.add_city(cls.activeCountry,city)
-    def update_handler(cls,cityName,field: dict):
-        cls.db.update_city(cls.activeCountry,cls.db.get_city(cls.activeCountry,cityName).title)
-    def delete_handler(cls,city=None):
+
+    def add_handler(cls, city):
+        cls.db.add_city(cls.activeCountry, city)
+
+    def update_handler(cls, cityName, field: dict):
+        cls.db.update_city(cls.activeCountry, cls.db.get_city(
+            cls.activeCountry, cityName).title)
+
+    def delete_handler(cls, city=None):
         if city:
             cls.db.delete_handler()
--- model_tests.py
+++ model_tests.py
@@ -1,59 +1,211 @@
 from db import *
 from model import *
+import pytest
+
 
 # -- Location tests --
-def test_location_init():
-    def normal_init():    
-        location1=Location("location1",(1,2))
-    def empty_title_init():
-        location2=Location("",(3,3))
-def test_location_todict():
-    pass
-def test_location_tost():
-    pass
+def location_helper():
+    normal_init = Location("location1", (1, 2))
+    return normal_init
+
+
+class TestLocation():
+    # def test_location_init():
+    def test_normal_init(self):
+        location1 = location_helper()
+        assert str(location1) == str(Location("location1", (1, 2)))
+
+    def test_empty_title_init(self):
+        with pytest.raises(TypeError) as e_info:
+            location2 = Location("", (3, 3))
+
+    def test_no_title_init(self):
+        with pytest.raises(TypeError) as e_info:
+            location3 = Location(None, (3, 3))
+
+    def test_negative_coord_init(self):
+        with pytest.raises(ValueError) as e_info:
+            location4 = Location("location4", (-1, -1))
+
+    def test_no_coord_init(self):
+        with pytest.raises(TypeError) as e_info:
+            location5 = Location("location5", None)
+            location6 = Location("location6", (None, None))
+
+    def test_empty_init(self):
+        with pytest.raises(TypeError) as e_info:
+            location7 = Location()
+
+    def test_location_todict(self):
+        location_dict = dict(location_helper())
+        assert location_dict == {'title': 'location1', 'coordinates': (1, 2)}
+
+    def test_location_tost(self):
+        location_str = str(location_helper())
+        assert location_str == "Location: location1, coordinates:(1, 2)"
 
 # -- Activity tests --
-def test_activity_init():
-    pass
-def test_activity_tostr():
-    pass
-def test_activity_iter():
-    pass
-def test_activity_todict():
-    pass
-def test_activity_fromdict():
-    pass
 
-# -- City tests --
-def test_city_init():
-    pass
-def test_city_append_activity():
-    pass
-def test_city_todict():
-    pass
-def test_city_iter():
-    pass
-def test_city_fromdict():
-    pass
-def test_city_to_json():
-    pass
+
+def activity_helper():
+    return Activity('normal_activity', (1, 2), 10)
+
+
+class TestActivity():
+    # def test_activity_init(self):
+    def test_normal_init(self):
+        activity1 = activity_helper()
+        assert str(
+            activity1) == 'Location: normal_activity, coordinates:(1, 2), cost:10'
+
+    def test_empty_str_init(self):
+        with pytest.raises(TypeError) as e_info:
+            activity2 = Activity("", (1, 2), 5)
+
+    def test_none_str_init(self):
+        with pytest.raises(TypeError) as e_info:
+            activity3 = Activity(None, (1, 2), 3)
+
+    def test_zero_cord_init(self):
+        activity4 = Activity("normal", (0, 0), 5)
+        assert str(activity4) == "Location: normal, coordinates:(0, 0), cost:5"
+
+    def test_neg_cord_init(self):
+        with pytest.raises(ValueError) as e_info:
+            activity5 = Activity("normal", (-1, -1), 5)
+
+    def test_no_cord_init(self):
+        with pytest.raises(TypeError) as e_info:
+            activity6 = Activity("normal", None, 5)
+            activity7 = Activity("normal", (None, None), 5)
+
+    def test_zero_cost_init(self):
+        activity8 = Activity('normal', (1, 1), 0)
+
+    def test_no_cost_init(self):
+        with pytest.raises(TypeError) as e_info:
+            activity9 = Activity('normal', (1, 1), None)
+
+    def test_neg_cost_init(self):
+        with pytest.raises(ValueError) as e_info:
+            activity10 = Activity('normal', (1, 1), -1)
+
+    def test_normal_activity_tostr(self):
+        # TODO abnormal tests
+        activity_str = str(activity_helper())
+        assert activity_str == "Location: normal_activity, coordinates:(1, 2), cost:10"
+
+    # def test_activity_iter(self):
+    #     iter_activity=iter(activity_helper())
+    #     #TODO abnormal activity_iter
+    #     assert type(iter_activity)== iter()
+
+    def test_activity_todict(self):
+        dict_activity = dict(activity_helper())
+
+        assert dict_activity == {'location': {
+            'title': 'normal_activity', 'coordinates': (1, 2)}, 'cost': 10}
+        # TODO abnormal activity_dict
+
+    def test_activity_fromdict(self):
+        activity_dict = Activity.from_dic(dict(activity_helper()))
+        assert dict(activity_dict) == dict(activity_helper())
+
+# -- City tests -- #
+
+
+def city_helper():
+
+    activity = Activity('secondactivity', (100, 100), 10)
+    return City('city', (0, 0), [activity_helper(), activity])
+
+
+class TestCity():
+    # def test_city_init(self):
+    def test_normal_city_init(self):
+        city1 = city_helper()
+        assert str(city1) == 'City: Location: city, coordinates:(0, 0), '\
+                             'Activities: '\
+                             '[Location: normal_activity, coordinates:(1, 2), cost:10, '\
+                             'Location: secondactivity, coordinates:(100, 100), cost:10]'
+
+    def test_city_init_one_activity(self):
+        location = location_helper()
+        city2 = City(location.title, location.coordinates, activity_helper())
+
+        assert str(city2) == 'City: Location: location1, coordinates:(1, 2), '\
+            'Activities: [Location: normal_activity, coordinates:(1, 2), cost:10]'
+
+    def test_city_init_no_activity(self):
+        city3 = City('location1', (1, 2), None)
+        assert str(city3) == 'City: Location: location1, coordinates:(1, 2), '\
+                             'Activities: [None]'
+
+    def test_city_init_no_location(self):
+        with pytest.raises(TypeError) as e:
+            city4 = City(None, activity_helper())
+
+    def test_city_append_activity(self):
+        city5 = city_helper()
+        activity = Activity('thirdactivity', (100, 100), 10)
+        city5.append_activity(activity)
+        assert str(city5) == 'City: Location: city, coordinates:(0, 0), '\
+            'Activities: [Location: normal_activity, coordinates:(1, 2), cost:10, '\
+            'Location: secondactivity, coordinates:(100, 100), cost:10, '\
+            'Location: thirdactivity, coordinates:(100, 100), cost:10]'
+
+    def test_city_todict(self):
+        city6 = dict(city_helper())
+        assert city6 == {'City':
+                         {'Location': {'title': 'city', 'coordinates': (0, 0)},
+                          'Activities': [
+                             {'location': {'title': 'normal_activity',
+                                           'coordinates': (1, 2)}, 'cost': 10},
+                             {'location': {'title': 'secondactivity',
+                                           'coordinates': (100, 100)}, 'cost': 10}]}}
+
+    def test_city_fromdict(self):
+        city8 = City.from_dic(dict(city_helper()))
+        assert dict(city8) == dict(city_helper())
+
+    # TODO Potentially wrong
+    def test_city_to_json(self):
+        city9 = city_helper().to_json()
+        assert city9 == {'City':
+                         {'Location': {'title': 'city', 'coordinates': [0, 0]},
+                          'Activities': [
+                             {'location': {'title': 'normal_activity',
+                                           'coordinates': [1, 2]}, 'cost': 10},
+                             {'location': {'title': 'secondactivity', 'coordinates': [100, 100]}, 'cost': 10}]}}
 
 # -- Country tests --
+
+
 def test_country_init():
     pass
+
+
 def test_country_update_city():
     pass
+
+
 def test_country_delete_city():
     pass
 
 # -- Model tests --
+
+
 def test_model_init():
-    pass 
+    pass
+
+
 def test_model_setactive():
     pass
+
+
 def test_model_listhandler():
     pass
 
 
 if __name__ == "__main__":
-    test_location_init()
+    pass
